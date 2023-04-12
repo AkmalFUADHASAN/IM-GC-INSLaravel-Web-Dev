f7c82d3b9499b0a62176d8e4eba6309290f370c1
unknown <akmalfuad7@gmail.com>
2023-04-12 13:34:32 +0700

    tugas laravel welcome.blade.php<akmal_fuad_hasan>

---
+++ Tugas 12/Kontak.blade.php
@@ -0,0 +1,32 @@
+<p>
+<h1>BUTUH BANTUAN? </h1> </p>
+ 
+	<p>Ini Adalah Halaman Kontak</p>
+
+	<style>
+  body {
+   background-color: lightblue;
+  }
+</style> 
+
+	<table border="1">
+		<tr>
+			<td>Email</td>
+			<td>:</td>
+			<td>akmalfuad7@gmail.com</td>
+		</tr>
+		<tr>
+			<td>Hp</td>
+			<td>:</td>
+			<td>0813-7959-6788 (Nomor telpon dan WA)</td>
+		</tr>
+		<tr>
+			<td>Alamat</td>
+			<td>:</td>
+			<td>Jalan Yossudarso Minas KM 30 kelurahan Minas Jaya Kabupaten Siak Provinsi Riau</td>
+		</tr>
+	</table>
+	
+	<footer>
+		<p>&copy; <a href=> @akmalfuad7.com</a>. 2023 - 2024</p>
+	</footer>
